9f767c362cd1171f3cc2b9d5b883b12a8d762a80
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2023-03-16 01:19:19 +0100

    add currentTime to video

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const volumeRange = document.querySelector('.volume-range');
 const volumeBar = document.querySelector('.volume-bar');
 const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
+let currentTime = document.querySelector('.time-elapsed');
 
 // Play & Pause -----------------------------------------
 
@@ -29,6 +30,25 @@ function togglePlay() {
 // On Video End, show play button icon
 video.addEventListener('ended', showPlayIcon);
 
+// Progress Barr ---------------------------------------
+
+// Calculate display time format
+function displayTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+}
+
+// Update progress bar as video plays
+function updateProgress() {
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)} `;
+}
+
 // Event Listeners
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
